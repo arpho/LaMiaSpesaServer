f738b52f964c5ab0f4f061667d9376401d690b75
D'Amico Giuseppe <damicogiuseppe77@gmail.com>
2014-04-11 14:29:24 +0200

    we can authenticate with local and facebook

--- app/config/passport.js
+++ app/config/passport.js
@@ -79,5 +79,71 @@ module.exports = function(passport) {
         });
 
     }));
+    // =========================================================================
+    // LOCAL SIGNUP ============================================================
+    // =========================================================================
+    passport.use('local-signup',new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done){
+		process.nextTick(function(){
+			User.findOne({ 'local.email' :  email },function(err,user){
+            // if there are any errors, return the error
+            if (err){
+                return done(err);
+               }
+            // check to see if theres already a user with that email
+            if (user) {
+				return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+            }
+            else{
+				// if there is no user with that email
+                // create the user
+                var newUser = new User();
+                newUser.local.email    = email;
+                newUser.local.password = newUser.generateHash(password);
+                newUser.save(function(err) {
+					if (err){ throw err;}
+                    return done(null, newUser);	
+				});
+
+            }
+                
+                });
+		});
+    }
+    ));
+    // =========================================================================
+    // LOCAL LOGIN =============================================================
+    // =========================================================================
+	// we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
+	passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },function(req, email, password, done){
+		User.findOne({ 'local.email' :  email },function(err,user){
+			if (err){
+				return done(err);
+				}
+			if (!user)
+				{return done(null, false, req.flash('loginMessage', 'No user found.'));}
+			if (!user.validPassword(password))
+				{return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));}
+			// all is well, return successful user
+            return done(null, user);
+
+			
+		});
+    }));
+    
+    // =========================================================================
+    // LOCAL LOGIN===================================================================
+    
 
 };
