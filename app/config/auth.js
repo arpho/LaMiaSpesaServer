40943f6f3dd7e0846382995102050a9a37029f0b
D'Amico Giuseppe <damicogiuseppe77@gmail.com>
2014-04-11 12:26:37 +0200

    added facebook's login button

--- app/config/auth.js
+++ app/config/auth.js
@@ -2,21 +2,21 @@
 module.exports = {
 
 	'facebookAuth' : {
-		'clientID' 		: '494748670648076', // your App ID
-		'clientSecret' 	: 'e7c4cdc433df2cd8e7bfc03c5124984c', // your App Secret
-		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+		'clientID'		: '494748670648076', // your App ID
+		'clientSecret'	: 'e7c4cdc433df2cd8e7bfc03c5124984c', // your App Secret
+		'callbackURL'	: 'http://localhost:8080/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
-		'consumerKey' 		: 'your-consumer-key-here',
-		'consumerSecret' 	: 'your-client-secret-here',
-		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+		'consumerKey'		: 'your-consumer-key-here',
+		'consumerSecret'	: 'your-client-secret-here',
+		'callbackURL'		: 'http://localhost:8080/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
-		'clientID' 		: 'your-secret-clientID-here',
-		'clientSecret' 	: 'your-client-secret-here',
-		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+		'clientID'		: 'your-secret-clientID-here',
+		'clientSecret'	: 'your-client-secret-here',
+		'callbackURL'	: 'http://localhost:8080/auth/google/callback'
 	}
 
 };
